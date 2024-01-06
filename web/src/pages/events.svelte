135a6f53bfbafed1d352cc52ae3945dad45c57e2
Jonathan Ting <jonathan.ting07@gmail.com>
2024-01-06 15:09:51 -0600

    Event table with images

--- web/src/pages/events.svelte
+++ web/src/pages/events.svelte
@@ -1,30 +1,23 @@
 <script>
-    import { Alert } from 'flowbite-svelte';
-    export let events;
-    let counter = 0;
-    let incCounter = () => counter++;
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+  import { Alert } from 'flowbite-svelte';
+  export let events;
+  let counter = 0;
+  let incCounter = () => counter++;
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+  import { Img } from 'flowbite-svelte';
 </script>
   <Table striped={true}>
     <TableHead>
-      <TableHeadCell>Product name</TableHeadCell>
-      <TableHeadCell>Color</TableHeadCell>
-      <TableHeadCell>Category</TableHeadCell>
-      <TableHeadCell>Price</TableHeadCell>
-      <TableHeadCell>
-        <span class="sr-only">Edit</span>
-      </TableHeadCell>
+      <TableHeadCell>Image</TableHeadCell>
+      <TableHeadCell>Event</TableHeadCell>
+      <TableHeadCell>Region</TableHeadCell>
     </TableHead>
     <TableBody class="divide-y">
     {#each events as event}
       <TableBodyRow>
+        <TableBodyCell><Img src="/static/event_imgs/{event.image_file}" alt="sample 1" size="h-16" class="rounded-lg" /></TableBodyCell>
         <TableBodyCell>{event.name}</TableBodyCell>
-        <TableBodyCell>Sliver</TableBodyCell>
-        <TableBodyCell>Laptop</TableBodyCell>
-        <TableBodyCell>$2999</TableBodyCell>
-        <TableBodyCell>
-          <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
-        </TableBodyCell>
+        <TableBodyCell>{event.region}</TableBodyCell>
       </TableBodyRow>
     {/each}
     </TableBody>
