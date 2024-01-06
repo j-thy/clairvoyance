e300116404fcaf81a18aae847816e39f75893138
Jonathan Ting <jonathan.ting07@gmail.com>
2024-01-06 13:37:00 -0600

    Added flowbite-svelte

--- web/src/pages/events.svelte
+++ web/src/pages/events.svelte
@@ -1,8 +1,15 @@
 <script>
+    import { Alert } from 'flowbite-svelte';
     export let events;
     let counter = 0;
     let incCounter = () => counter++;
 </script>
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
 <p>Hello from Svelte!
     <span style="color: transparent">
         <span class="text-blue-500">and Tailwind!</span>
