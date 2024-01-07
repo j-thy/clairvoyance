a1c0f7c7bf10fc9f84cb459cf3fdd162ceb3f2e5
Jonathan Ting <jonathan.ting07@gmail.com>
2024-01-07 11:25:44 -0600

    Added DaisyUI

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,10 @@
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin'),
+    require("daisyui"),
+  ],
 
   darkMode: 'class',
 
