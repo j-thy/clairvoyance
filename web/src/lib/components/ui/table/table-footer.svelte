00b862c43beefe80452dbceeaf587cea001c351b
Jonathan Ting <jonathan.ting07@gmail.com>
2024-11-01 18:34:31 -0400

    Add shadcn components to git

---
+++ web/src/lib/components/ui/table/table-footer.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { cn } from "$lib/utils.js";
+
+	let { ref = $bindable(null), class: className, children, ...restProps } = $props();
+</script>
+
+<tfoot
+	bind:this={ref}
+	class={cn("bg-primary text-primary-foreground font-medium", className)}
+	{...restProps}
+>
+	{@render children?.()}
+</tfoot>
